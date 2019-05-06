85033849aac2b39c91a6eba60bd59ec9578efd89
Francisco Alvarez <faove@hotmail.com>
2019-05-06 14:07:40 -0400

    probando las ramas

--- menu_principal.php
+++ menu_principal.php
@@ -2,7 +2,7 @@
 <?php
 //header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 //header("Cache-Control: post-check=0, pre-check=0", false);
-//header("Pragma: no-cache"); shuffle
+//header("Pragma: no-cache"); shuffle cambio branch
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US" xml:lang="en">
 	<head>
